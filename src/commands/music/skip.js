0f27058a60b5c4071a9ceb21837f9d0f62749aa6
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-05-27 01:20:53 +0200

    Add basic /skip command

---
+++ src/commands/music/skip.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { useMasterPlayer } = require('discord-player');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('skip')
+        .setDescription('Skips the current song'),
+    async execute(interaction, client) {
+        const player = useMasterPlayer();
+
+        const queue = player.nodes.get(interaction.guildId);
+
+        if (!queue)
+            return await interaction.reply('There are no songs in the queue!');
+
+        const skippedName = queue.currentTrack.title;
+        queue.node.skip();
+        await interaction.reply(`Skipped **${skippedName}**.`);
+    }
+}
