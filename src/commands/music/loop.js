5d096c57feb2c0220d8c7360f3c21ff2bcf55ac3
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-05-27 15:03:24 +0200

    Fix other files with broken isPlaying checks

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -22,7 +22,7 @@ module.exports = {
 
         let modeName = ['off', 'music loop', 'queue loop', 'autoplay'][mode];
 
-        if (!queue || !queue.node.isPlaying)
+        if (!queue || !queue.node.isPlaying())
             return await interaction.reply('There are no songs in the queue!');
 
         queue.setRepeatMode(mode);
