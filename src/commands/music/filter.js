b32af6df61570eea54fbd7055d25e96ed1e5de89
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-05-27 15:17:08 +0200

    Add 8D filter

--- src/commands/music/filter.js
+++ src/commands/music/filter.js
@@ -6,22 +6,27 @@ module.exports = {
         .setName('filter')
         .setDescription('Toggles an audio filter')
         .addStringOption(option =>
-			option.setName('filter')
-				.setDescription('The filter you want to toggle')
-				.setRequired(true)
-				.addChoices(
-					{ name: 'bass boost', value: 'bassboost' },
-					{ name: 'nightcore', value: 'nightcore' },
-				)),
+            option.setName('filter')
+                .setDescription('The filter you want to toggle')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'bass boost', value: 'bassboost' },
+                    { name: 'nightcore', value: 'nightcore' },
+                    { name: '8D', value: '8D' },
+                )),
     async execute(interaction, client) {
         const player = useMasterPlayer();
         const queue = player.nodes.get(interaction.guildId);
         const filter = interaction.options.getString('filter');
 
-        if(!queue || !queue.node.isPlaying())
+        if (!queue || !queue.node.isPlaying())
             return await interaction.reply('There are no songs in the queue!');
 
-        await queue.filters.ffmpeg.toggle([ filter ]);
+        if (filter != '8D')
+            await queue.filters.ffmpeg.toggle([filter]);
+        else
+            await queue.filters.filters.setFilters([filter]);
+
         await interaction.reply(`Toggled **${filter}** filter on the queue.`);
     }
 }
