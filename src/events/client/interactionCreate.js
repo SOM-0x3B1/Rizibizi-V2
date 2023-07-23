7df37d1ea55e803bfa8d3eedcf7c44435d82dbe2
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-07-23 15:14:54 +0200

    Add playlist creation from queue

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,5 +1,8 @@
 const { dbPool, valueExists } = require('../../db.js');
-var crypto = require('crypto');
+const { useMainPlayer } = require('discord-player');
+const crypto = require('crypto');
+
+const newSongQuery = "INSERT INTO song(url,playlistID,type,position) VALUES (?, ?, ?, ?)";
 
 module.exports = {
     name: 'interactionCreate',
@@ -37,14 +40,34 @@ module.exports = {
                 const editKey = crypto.randomBytes(8).toString('hex');
                 const values = [id, editKey, playlistName, playlistDesc, interaction.user.id, interaction.guildId, interaction.user.username];
                 await conn.query("INSERT INTO playlist(id,editKey,name,description,editorID,guildID,editorName) VALUES(?, ?, ?, ?, ?, ?, ?)", values);
-                await interaction.reply(`:page_with_curl: Playlist named **${playlistName}** created successfully. \n Global playlist ID: **${id}**`);                
+
+                if (interaction.customId === 'queuePlaylistCreator') {
+                    const player = useMainPlayer();
+                    const queue = player.nodes.get(interaction.guildId);
+
+                    const currentSong = queue.currentTrack;
+                    await conn.query(newSongQuery, [currentSong.url, id, urlToType(currentSong.url), 0]);
+                    for (let i = 0; i < queue.tracks.size; i++) {
+                        let song = queue.tracks.data[i];
+                        await conn.query(newSongQuery, [song.url, id, urlToType(song.url), i + 1]);
+                    }
+                }
+
+                await interaction.reply(`:page_with_curl: Playlist named **${playlistName}** created successfully. \n Global playlist ID: **${id}**`);
             }
-            else{
+            else {
                 await interaction.reply({
                     content: `:warning: A playlist named ${playlistName} already exists.`,
                     ephemeral: true
                 });
-            }            
+            }
         }
     }
+}
+
+async function urlToType(url) {
+    if (url.includes('youtube'))
+        return 'youtubeVideo';
+    else
+        return 'idk'
 }
