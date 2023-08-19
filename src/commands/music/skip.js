66f128fdfbd75a8eb18cdbd6f7136b741ab208ea
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-08-19 22:35:59 +0200

    Fiy and further improve /skip

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -4,7 +4,8 @@ const { useMainPlayer } = require('discord-player');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('skip')
-        .setDescription('Skips the current song'),
+        .setDescription('Skips the current song')
+        .addNumberOption((option) => option.setName('to_index').setDescription('Index of the song in the queue to skip to.').setMinValue(1)),
     async execute(interaction, client) {
         const player = useMainPlayer();
         const queue = player.nodes.get(interaction.guildId);
@@ -12,8 +13,17 @@ module.exports = {
         if (!queue || !queue.currentTrack)
             return await interaction.reply(':warning: There are no songs in the queue.');
 
-        //const skippedName = queue.currentTrack.title;
+        const index = interaction.options.getNumber('to_index') ?? 1;
+        if (index > 1) {
+            let max = queue.tracks.size - 1;
+            for (let i = 0; i < index - 1 && i < max; i++)
+                await queue.removeTrack(0);
+        }
         await queue.node.skip();
-        await interaction.reply(`:track_next: Skipped to **${queue.tracks.data[0].title}**.`);
+
+        if (queue.tracks.size > 0)
+            await interaction.reply(`:track_next: Skipped to **${queue.tracks.data[0].title}**.`);
+        else
+            await interaction.reply(`:track_next: Skipped the last song.`);
     }
 }
